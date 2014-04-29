6f05b1f7b7b51492435c4f13f32e2ad50ca5a7e8
Janet Chuang <janet.chuang@gmail.com>
2014-04-29 11:26:54 -0400

    set up Artist class for API usage

--- lib/last_ruby_fm.rb
+++ lib/last_ruby_fm.rb
@@ -1,4 +1,5 @@
-require "last_ruby_fm/version"
+require 'last_ruby_fm/version'
+require 'last_ruby_fm/artist'
 
 module LastRubyFm
   # Your code goes here...
---
+++ lib/last_ruby_fm/artist.rb
@@ -0,0 +1,36 @@
+require 'faraday'
+require 'json'
+
+module LastRubyFM
+  class Artist
+    def initialize(attributes)
+    end
+
+    class << self
+      def search(phrase)
+        res = connection.get('/2.0/', {
+          method: 'artist.search',
+          artist: phrase,
+          api_key: LastRubyFm.api_key,
+          format: 'json'
+        })
+        parsed_results = JSON.parse(res.body)
+        if parsed_results['results'] && parsed_results['results']['artistmatches']
+          parsed_results['results']['artistmatches']['artist'].map do |artist_attributes|
+            new(artist_attributes)
+          end
+        else
+          []
+        end
+      end
+
+      protected
+      def connection
+        conn ||= Faraday.new(:url => 'http://ws.audioscrobbler.com') do |faraday|
+          faraday.request :url_encoded
+          faraday.adapter Faraday.default_adapter
+        end
+      end
+    end
+  end
+end
