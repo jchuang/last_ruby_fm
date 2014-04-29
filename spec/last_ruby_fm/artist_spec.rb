fa283885fecc6b5f51c3c1dab75ee96eb0f995dd
Janet Chuang <janet.chuang@gmail.com>
2014-04-29 11:13:01 -0400

    add artist spec and spec_helper

---
+++ spec/last_ruby_fm/artist_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe LastRubyFm::Artist do
+  context 'searching' do
+
+    it 'returns a list of relevant artists' do
+      artist_name = 'Stone Temple Pilots'
+      results = LastRubyFm::Artist.search(artist_name)
+
+      expect(results.size).to_not eql(0)
+      expect(results[0]).to be_kind_of(LastRubyFm::Artist)
+      expect(results[0].name).to eql(artist_name)
+    end
+  end
+end
